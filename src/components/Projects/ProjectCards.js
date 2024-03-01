1ab6d98d7037a765695ee2f0a134111106112069
Kamronp23 <kamronpoosti@gmail.com>
2024-03-01 11:46:36 -0800

    fixed card spacing for projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,34 +8,35 @@ function ProjectCards(props) {
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <AiFillGithub /> &nbsp;
-          GitHub
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+      <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
+        <div>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify", wordWrap: 'break-word', textAlign: 'center' }}>
+            {props.description}
+          </Card.Text>
+        </div>
+        <div>
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <AiFillGithub /> &nbsp;
+            GitHub
           </Button>
-        )}
+          {"\n"}
+          {"\n"}
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;
+              {"Demo"}
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
-
   );
 }
+
 export default ProjectCards;
